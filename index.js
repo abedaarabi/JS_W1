f7dac9c039079671bcbf83e055882ef449cb007b
Abd Aarabi <abedaarabi@gmail.com>
2019-05-21 09:53:04 +0200

    add readme file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Age-ify (A future age calculator)
+
 const yearOfBirth = 1991
 const yearFuture = 2027
 const futureYear = yearFuture - yearOfBirth
 
 console.log(futureYear)
+console.log('I WILL BE IN THE ' + futureYear + 'YEARS OLD IN ' + yearFuture) ;
---
+++ readme.md
@@ -0,0 +1,5 @@
+#My fist home work 
+Age-ify (A future age calculator
+Goodboy-Oldboy (A dog age calculator)
+Housey pricey (A house price estimator)
+Ez Namey (Startup name generator) Optional
